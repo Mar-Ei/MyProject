f82e1aef1881e28a59bffeff6bf224888e98e3e1
Mar-Ei <elena_s_05@mail.ru>
2024-10-20 23:57:45 +0300

    up

---
+++ BackendApi2/BackendApi/BackendApi/Program.cs
@@ -0,0 +1,61 @@
+
+using Domain.Interfaces;
+using BusinessLogic.Services;
+using DataAccess.Wrapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using BackendApi.Models;
+
+namespace BackendApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddDbContext<MedicalContext>(options =>
+            options.UseSqlServer(builder.Configuration["ConnectionString"]));
+
+
+            builder.Services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
+            builder.Services.AddScoped<IUserService, UserService>();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Медицинские данные",
+                });
+                var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+            });
+
+            var app = builder.Build();
+         //   using (var scope = app.Services.CreateScope())
+         //   {
+           //     var services = scope.ServiceProvider;
+
+           //     var context = services.GetRequiredService<MedicalContext>();
+          //      context.Database.Migrate();
+          //  }
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
